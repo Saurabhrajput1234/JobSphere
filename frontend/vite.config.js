deeb2c22743c07db2b6e19676a993cdc6f9dcd19
saurabh <saurabh2113076@akgec.ac.in>
2025-06-08 20:41:22 +0530

    use materialUI

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,17 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': '/src',
+    },
+  },
+  build: {
+    rollupOptions: {
+      external: ['@mui/material', '@emotion/react', '@emotion/styled'],
+    },
+  },
+  optimizeDeps: {
+    include: ['@mui/material', '@emotion/react', '@emotion/styled'],
+  },
 })
